5565a288b5c75eddcf1abc4e8a934faa18a9c04d
nebil <mhmdahmdnbyl18@gmail.com>
2023-11-13 12:48:58 +0100

    démonstrateur : Done

--- démonstrateur/src/Main.java
+++ démonstrateur/src/Main.java
@@ -5,22 +5,25 @@ public class Main
 {
     public static void main(String[] args) {
      
-        String url = "jdbc:oracle:thin:@oracle1.ensimag.fr:1521:orcale1";
+        String url = "jdbc:oracle:thin:@oracle1.ensimag.fr:1521:oracle1";
         // "jdbc:oracle:thin:@oracle1.ensimag.fr:1521:nom_bd"
         String user="mohameml";
         String password = "mohameml";
-
+    
         try 
         {
 
             // l'énregistrement du pilote driver d'orcale :
+            System.err.println("enregistrement du  pilote .... ");
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver()); 
+            System.out.println("loaded ....");
 
             // la connexion:
+            System.out.println("connexion ......");
             Connection connection =  DriverManager.getConnection(url, user, password);
         
             // préparation de la réquette :
-            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Comptes WHERE SOLDE ?");
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Comptes WHERE SOLDE = ?");
 
             Scanner sc = new Scanner(System.in);
             
@@ -50,3 +53,5 @@ public class Main
 
     }
 }
+
+
